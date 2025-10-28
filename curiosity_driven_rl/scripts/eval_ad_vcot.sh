31143ddc4a924cce19cc1643e6c766e0b7315439
MEKSAAA <2994735278@qq.com>
2025-10-29 04:19:33 +0800

    fix(data): update test dataset

--- curiosity_driven_rl/scripts/eval_ad_vcot.sh
+++ curiosity_driven_rl/scripts/eval_ad_vcot.sh
@@ -1,9 +1,8 @@
-benchmark=test8028-crossdataset102821
+benchmark=cross8028
 export working_dir="/NEW_EDS/miaojw/projects/Pixel-Reasoner/curiosity_driven_rl"
 
-#export policy="/NEW_EDS/miaojw/projects/Pixel-Reasoner/output/ad_sft_qwen25vl7b_1024/checkpoint-88"
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-export policy=""
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+export policy="/NEW_EDS/miaojw/projects/Pixel-Reasoner/output/cross_sft_qwen25vl3b1029/checkpoint-198"
 export savefolder=eval
 export nvj_path="/NEW_EDS/miaojw/miniconda3_new/envs/curiosity9/lib/python3.10/site-packages/nvidia/nvjitlink/lib"
 ############
@@ -11,7 +10,7 @@ export sys=anomaly_vcot # define the system prompt
 export MIN_PIXELS=401408
 export MAX_PIXELS=4014080 # define the image resolution
 export eval_bsz=128 # vllm will processes this many queries 
-export tagname=eval_crossdataset_3b88_102821
+export tagname=eval_cross_3b198_1029
 
 export testdata="/NEW_EDS/miaojw/projects/Pixel-Reasoner/ad-dt/test8028cross.json"
 export num_vllm=4
